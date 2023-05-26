c2247df50421db166a427430f434c7e642c64616
Glenn Tippett <glenn.tippett@ovoenergy.com.au>
2023-05-26 17:04:50 +1000

    feat(tools): WIP: custom nvim config

---
+++ nvim/custom/init.lua
@@ -0,0 +1,13 @@
+require("user.plugins-setup") -- Keep at the top
+require("user.core.options")
+require("user.core.keymaps")
+require("user.core.colorscheme")
+require("user.plugins.comment")
+require("user.plugins.nvim-tree")
+require("user.plugins.lualine")
+require("user.plugins.telescope")
+require("user.plugins.nvim-cmp")
+require("user.plugins.lsp.mason")
+require("user.plugins.lsp.lsp-saga")
+require("user.plugins.lsp.lsp-config")
+
