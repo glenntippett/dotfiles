b8ab1845fe29f4dd1e1a8c070035303ca855498b
Glenn Tippett <glenn.tippett@ovoenergy.com.au>
2023-05-29 10:37:14 +1000

    add descriptions to keymaps

--- nvim/custom/lua/user/core/keymaps.lua
+++ nvim/custom/lua/user/core/keymaps.lua
@@ -5,9 +5,6 @@ local keymap = vim.keymap
 -- mode  key   replaces
 keymap.set("n", "x", "_x") -- When removing a single character don't copy it
 
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
@@ -20,20 +17,25 @@ keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "Next tab" })
 keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "Previous tab" })
 
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 
 -- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find FILES in current working directory" }) -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find STRING in current working directory" }) -- find string in current working directory as you type
+keymap.set(
+	"n",
+	"<leader>fc",
+	"<cmd>Telescope grep_string<cr>",
+	{ desc = "Find string under cursor in current working directory" }
+) -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "List open buffers in current instance" }) -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "List help tags" }) -- list available help tags
 
 -- telescope git commands
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = "List all commits" }) -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>", { desc = "List commits for current buffer" }) -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { desc = "List branches" }) -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { desc = "List file changes" }) -- list current changes per file with diff preview ["gs" for git status]
 
 -- restart lsp server
-keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart LSP" }) -- mapping to restart lsp if necessary
