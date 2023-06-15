9c235b9769d51cc40e0587d2b98446258cde8aa6
Glenn <48549970+glenntippett@users.noreply.github.com>
2023-06-15 12:59:00 +1000

    new wallpaper

--- ubuntu/scripts/set_wallpaper.sh
+++ ubuntu/scripts/set_wallpaper.sh
@@ -1,1 +1,1 @@
-gsettings set org.gnome.desktop.background picture-uri file:///home/glenn/dotfiles/ubuntu/wallpaper.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///home/glenn/dotfiles/ubuntu/deathstar.png
