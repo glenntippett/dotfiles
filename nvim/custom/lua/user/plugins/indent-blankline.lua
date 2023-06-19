e9a7fbbe0c22e6b373bd7494913813e4e0f9059d
Glenn Tippett <glenn.tippett@ovoenergy.com.au>
2023-06-19 10:01:39 +1000

    remove indent line colours

--- nvim/custom/lua/user/plugins/indent-blankline.lua
+++ nvim/custom/lua/user/plugins/indent-blankline.lua
@@ -3,13 +3,6 @@ if not status then
 	return
 end
 
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
-
 indent_blankline.setup({
 	space_char_blankline = " ",
 	show_current_context = true,
@@ -27,13 +20,5 @@ indent_blankline.setup({
 		"^table",
 		"block",
 		"arguments",
-	},
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
 	},
 })
