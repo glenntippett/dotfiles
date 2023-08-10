e0a4514032b1c4f735a309b666a98c48094b5042
Glenn Tippett <glenn.tippett@ovoenergy.com.au>
2023-08-10 16:23:38 +1000

    gst

--- macos/paths.sh
+++ macos/paths.sh
@@ -1,5 +1,3 @@
 export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
-export PATH="/opt/homebrew/bin/tfswitch/bin:$PATH"
+export PATH="~/bin:$PATH"
 
-# Mercurial - used if DiffView for Neovim
-export PATH="/opt/homebrew/bin/hg:$PATH"
