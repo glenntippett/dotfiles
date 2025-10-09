aaecb0eae32356eda34089a6fb2ac24580c12b63
Glenn Tippett <48549970+glenntippett@users.noreply.github.com>
2025-10-10 10:24:44 +1100

    rejig nvim dirs

---
+++ neovim/lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "sainnhe/everforest",
+    config = function()
+      vim.g.everforest_background = "hard"
+    end,
+  },
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+    end,
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "pineapplegiant/spaceduck" },
+  { "mocte4/godotcolour-vim" },
+  { "shaunsingh/nord.nvim" },
+  { "marko-cerovac/material.nvim" },
+  { "doom-neovim/doom-nvim" },
+  { "Shatur/neovim-ayu" },
+}
