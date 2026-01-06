2c58f94a651e16a43f5c3eb6524747cc1d6f9ce1
Glenn Tippett <48549970+glenntippett@users.noreply.github.com>
2026-01-06 16:04:50 +1100

    symlink new lazyvim config

---
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "sainnhe/everforest",
+    config = function()
+      vim.g.everforest_background = "hard"
+    end,
+  },
+}
---
+++ neovim/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,32 @@
+return {
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+
+                                        .
+                              .-o
+                 .           /  |
+        .                 . /   |   .
+                           /    |
+                  .       /     |
+  .                      /      /         .
+             .          /    _./   .
+                   _.---~-.=:_
+                  (_.-=() <~`-`-.
+                 _/ _() ~`-==-._,>
+         ..--====--' `~-._.__()
+     o===''~~             |__()
+                .         \   |             .
+                           \  \
+                            \  \     .
+        .                    \  \           Sienar Fleet Systems
+                 .            \  \          Lambda-class Imperial Shuttle (3)
+                               \_ \        .
+                       LS        ~o
+ ]],
+      },
+    },
+  },
+}
