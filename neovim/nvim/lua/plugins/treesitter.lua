fcace547e74d411a754abc95b48d3dc38b7bd23d
Glenn Tippett <48549970+glenntippett@users.noreply.github.com>
2026-01-29 08:09:44 +1100

    add comments

--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     highlight = {
+      -- enable/disable for all
       enable = false,
+      -- disable for specific languages
+      -- disable = { 'markdown' }
     },
   },
 }
