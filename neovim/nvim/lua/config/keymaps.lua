d6bdd68dd7e8c9a9c79ce46d7be9bcab01cf8302
Glenn Tippett <48549970+glenntippett@users.noreply.github.com>
2026-08-18 09:30:07 +0800

    lazyvim plugins

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- https://github.com/smjonas/inc-rename.nvim
+-- Provides a command for LSP renaming with immediate
+-- visual feedback thanks to Neovim's command preview feature
+vim.keymap.set("n", "<leader>rn", ":IncRename ")
